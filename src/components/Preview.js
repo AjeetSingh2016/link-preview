22387c83cb2c188abfa5df8875eb3dc1a8ce7960
Ajeet Singh <80536741+AjeetSingh2016@users.noreply.github.com>
2022-08-08 03:50:57 +0530

    Update Preview.js

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -59,7 +59,7 @@ const Preview = ({image, title, description, link}) => {
           mt={{xs:"20px", sm:"7px"}}
           pb="10px"
         >
-          <a href={link}>
+          <a href=target="_blank" rel="noreferrer">
             {link}
           </a>
         </Typography>
